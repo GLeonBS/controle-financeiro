16ce9fbdd4c985dce45eeb663fb936ba7fff6e4e
Leon <gabriel.leonbs@gmail.com>
2023-12-28 13:44:49 -0300

    fix: 🐛 Ajust standalone project front

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
--- web/src/app/home/home.module.ts
+++ web/src/app/home/home.module.ts
@@ -1,7 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-
 
 const routes: Routes = [
   {
@@ -13,8 +13,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    RouterModule.forChild(routes),
-  ],
-  providers: [],
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
 })
 export class HomeModule { }
