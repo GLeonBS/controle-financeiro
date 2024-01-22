d3f345fcda9e1be24b3d25a52dada2d192e79f7b
Leon <gabriel.leonbs@gmail.com>
2024-01-22 12:38:49 -0300

    fix: 🐛 Fixing bean repository

--- server/src/main/java/com/example/controlefinanceiro/interfaces/RepositoryCRUD.java
+++ server/src/main/java/com/example/controlefinanceiro/interfaces/RepositoryCRUD.java
@@ -1,9 +1,9 @@
 package com.example.controlefinanceiro.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-
-@Repository
+@NoRepositoryBean
 public interface RepositoryCRUD<T, R> extends JpaRepository<R, T> {
+
 }
--- server/src/main/java/com/example/controlefinanceiro/repository/UsuarioRepository.java
+++ server/src/main/java/com/example/controlefinanceiro/repository/UsuarioRepository.java
@@ -1,7 +1,10 @@
 package com.example.controlefinanceiro.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.example.controlefinanceiro.domain.Usuario;
 import com.example.controlefinanceiro.interfaces.RepositoryCRUD;
 
+@Repository
 public interface UsuarioRepository extends RepositoryCRUD<Long, Usuario> {
 }
