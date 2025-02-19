459fbbb4caed4baa87ef5c8d9cfe16af0573d014
GLeonBS <gabriel.leonbs@gmail.com>
2025-02-19 12:14:02 -0300

    fix: 🐛 Ajuste de teste

--- server/src/test/java/br/com/controle_financeiro/modules/usuario/repository/UsuarioRepositoryTest.java
+++ server/src/test/java/br/com/controle_financeiro/modules/usuario/repository/UsuarioRepositoryTest.java
@@ -13,7 +13,7 @@ import config.ContainerEnviroment;
 import config.IntegrationTest;
 
 @IntegrationTest
-@Sql(statements = "INSERT INTO usuario (id, nome, email, data_nascimento, senha, roles, created_at) VALUES ('50b558b6-806e-414e-8bee-79b0e06ea684', 'Teste', 'teste@teste.com', '2021-01-01','123456', '{ADMIN}', '2025-01-20 16:35:00');")
+@Sql(statements = "INSERT INTO usuario (id, nome, email, data_nascimento, senha, roles, created_at) VALUES ('50b558b6-806e-414e-8bee-79b0e06ea684', 'Teste', 'teste@teste.com', '2021-01-01','123456', '{ROLE_ADMIN, ROLE_USER}', '2025-01-20 16:35:00');")
 class UsuarioRepositoryTest extends ContainerEnviroment {
 
     @Autowired
